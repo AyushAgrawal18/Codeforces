f68f93e4125b44bb9fe1baccf9ee16e23b952bd4
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-12 02:03:08 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main() {
+    fastio();
+        int m,n;
+        cin>>m>>n;
+        cout<<(m*n)/2<<endl;
+}
