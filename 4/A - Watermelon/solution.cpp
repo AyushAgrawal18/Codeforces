a89ba4616d4351290f853207f81c7e52632533ba
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-12 02:02:53 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main(){
+    int w;
+    cin>>w;
+    if((w%2==0)&&w!=2){
+            cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
