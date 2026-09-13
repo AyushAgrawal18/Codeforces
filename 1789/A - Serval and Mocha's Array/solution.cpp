48db468f144fd015a6e87f896231b6182b151636
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-14 00:31:11 +0530

    Add Serval and Mocha's Array [A] from Codeforces

---
+++ 1789/A - Serval and Mocha's Array/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+ 
+signed main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int flag=0;
+ 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(__gcd(a[i],a[j])<=2 && i!=j){
+                    flag=1;
+                }
+            }
+        }
+ 
+        if(flag==1){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+ 
+return 0;
+}
