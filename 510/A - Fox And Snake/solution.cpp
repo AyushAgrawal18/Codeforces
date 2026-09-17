4cd9f05672f54d67745e16001b27db8d3325e7c5
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-17 22:12:46 +0530

    Add Fox And Snake [A] from Codeforces

---
+++ 510/A - Fox And Snake/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main() {
+    fastio();
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(i%2!=0){
+                cout<<"#";
+            }
+            else{
+                if(i%4!=0){
+                    if(j<m){
+                        cout<<".";
+                    }
+                    else if(j==m){
+                        cout<<"#";
+                    }
+                }
+                else{
+                    if(j==1){
+                        cout<<"#";
+                    }
+                    else if(j>1&&j<=m){
+                        cout<<".";
+                    }
+                }
+            }
+        }
+        cout<<endl;
+    }
+    }
