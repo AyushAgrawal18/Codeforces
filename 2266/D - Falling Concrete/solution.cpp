68a99f471bfb258ca1155b26aa257e579e788940
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-21 20:56:15 +0530

    Add Falling Concrete [D] from Codeforces

---
+++ 2266/D - Falling Concrete/solution.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* 
+  ****************************************************
+  *                                                  *
+  *             COMPETITIVE PROGRAMMING              *
+  *                                                  *
+  *            Author: Ayush Kumar Agrawal           *
+  *                  Code Smart, Win Big             *
+  *                                                  *
+  ****************************************************
+*/
+ 
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((int)(v).size())
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+#define loop for(int i = 0; i < n; i++)
+#define rloop for(int i = n-1; i >= 0; i--)
+#define yes() cout << "YES
+"
+#define no() cout << "NO
+"
+ 
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const double PI = acos(-1);
+ 
+ 
+ 
+inline void solve() {
+    // Your solution goes here
+    ll n;
+    cin>>n;
+    vll b(n);
+    loop{
+        ll a;
+        cin>>a;
+        b[i]=a-(i+1);
+    }
+    sort(all(b));
+    b.erase(unique(b.begin(),b.end()),b.end());
+    ll ans=1,cur=1;
+    for(int i=1;i<b.size();i++){
+        if(b[i]==b[i-1]+1) cur++;
+        else cur=1;
+        ans=max(ans,cur);
+    }
+    cout<<ans<<endl;
+    
+}
+ 
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    while (t--) 
+        solve();
+    return 0;
+}
