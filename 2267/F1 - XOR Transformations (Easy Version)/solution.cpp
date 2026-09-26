acbd71c5b0823ed1749d0c3eb5243c4960fe548c
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-26 16:54:16 +0530

    Add XOR Transformations (Easy Version) [F1] from Codeforces

--- 2267/F1 - XOR Transformations (Easy Version)/solution.cpp
+++ 2267/F1 - XOR Transformations (Easy Version)/solution.cpp
@@ -46,9 +46,9 @@ inline void solve() {
     loop cin>>a[i];
     ll maxi= *max_element(all(a));
     ll mini= *min_element(all(a));
-    vll ans(62);
+    vll ans(33);
     ans[0]=maxi-mini;
-    for(int k=1;k<61;k++){
+    for(int k=1;k<32;k++){
     vll v;
         loop{
             for (int j=i+1;j<n;j++) {
@@ -63,7 +63,7 @@ inline void solve() {
     while(q--){
         int x;
         cin>>x;
-        if(x>60) cout<<0<<endl;
+        if(x>32) cout<<0<<endl;
         else cout<<ans[x]<<endl;
     }
 }
